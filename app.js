f95127f052f9f5f77cab0a545cc88116cd5b6d5f
Harry <harryle.au@gmail.com>
2018-05-31 18:59:27 +1000

    handle auth with passport-google-oauth2

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Passport config
+require('./config/passport')(passport);
+
+// Load Routes
+const auth = require('./routes/auth');
 
 const app = express();
 
@@ -8,6 +15,9 @@ const port = process.env.PORT || 5000;
 app.get('/', (req, res) => {
   res.send('index');
 });
+
+// Use Routes
+app.use('/auth', auth);
 
 app.listen(port, () => {
   console.log('started up on port', port);
---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI: 'mongodb://harryleau:Cuong2202!@ds141870.mlab.com:41870/storybooks-dev',
+  googleClientID: '999332707023-fqvep9pgrr2qggapu7u7jb3b4sf70t03.apps.googleusercontent.com',
+  googleClientSecret: 'SpPQAtjFoxf2H5Wj7DkgKFk_'
+}
+
+// googleClientID and googleClientSecret got from console.google, enable Google+ API and create a new project for web app to get credentials.
