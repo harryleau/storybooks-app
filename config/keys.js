6dd22307d16bcf7f4efb47459bcc71376c666f4f
Harry <harryle.au@gmail.com>
2018-06-01 23:39:48 +1000

    finished app, add config for heroku, ready to deploy

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://harryleau:Cuong2202!@ds141870.mlab.com:41870/storybooks-dev',
-  googleClientID: '999332707023-fqvep9pgrr2qggapu7u7jb3b4sf70t03.apps.googleusercontent.com',
-  googleClientSecret: 'SpPQAtjFoxf2H5Wj7DkgKFk_'
+if(process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
-
-// googleClientID and googleClientSecret got from console.google, enable Google+ API and create a new project for web app to get credentials.
