371d66d9ec12703ff0236f4937ffce2c8dbb3a83
Harry <harryle.au@gmail.com>
2018-06-01 22:46:37 +1000

    finish UI, add helpers methods, finish all functionalities, only listing comments left

---
+++ helpers/hbs.js
@@ -0,0 +1,24 @@
+const moment = require('moment');
+
+module.exports = {
+  truncate: function(str, len) {
+    if(str.length > len && str.length > 0) {
+      let newStr = str + ' ';
+      newStr = str.substr(0, len);
+      newStr = str.substr(0, newStr.lastIndexOf(' '));
+      newStr = (newStr.length > 0) ? newStr : str.substr(0, len);
+      return newStr + '...';
+    }
+    return str;
+  },
+  stripTags: function(input) {
+    // remove html tags
+    return input.replace(/<(?:.|\n)*?>/gm, '');
+  },
+  formatDate: function(date, format) {
+    return moment(date).format(format);
+  },
+  select: function(selected, options) {
+    return options.fn(this).replace(new RegExp(' value=\"' + selected + '\"'), '$&selected="selected"').replace(new RegExp('>' + selected + '</option>'), 'selected="selected"$&');
+  }
+}
